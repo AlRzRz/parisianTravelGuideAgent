0a0d9c5e3f202809213d8567e06efba23c2148f3
Prometheus <105338051+AlRzRz@users.noreply.github.com>
2025-09-03 02:53:44 +0400

    Hardcoded examples

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
+from openai import OpenAI
 
 
 def main():
-    pass
 
+    model = "gpt-4o-mini"
+
+    client = OpenAI()
+
+    system_prompt = """You are an AI-powered travel guide for the culturally rich city of Paris."""
+    conversation = [{"role": "system", "content": system_prompt}]
+
+    userMessages = [
+        {"role": "user", "content": 'How far away is the Louvre from the Eiffel Tower (in miles) if you are driving?'},
+        {"role": "user", "content": "Where is the Arc de Triomphe?"},
+        {"role": "user", "content": "What are the must-see artworks at the Louvre Museum?"}
+    ]
+
+
+    for message in userMessages:
+        conversation.append(message)
+        
+        completion = client.chat.completions.create(
+            model=model,
+            messages=conversation,
+            temperature=0,
+            max_completion_tokens=100
+        )
+
+        print(completion.choices[0].message.content)
+        
+        assistant_resp = {"role": "assistant", "content": completion.choices[0].message.content}
+        conversation.append(assistant_resp)
 
 
 if __name__ == '__main__':
